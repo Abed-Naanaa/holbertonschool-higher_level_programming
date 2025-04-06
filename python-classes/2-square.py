c8499807e79371e6ffca18fe50c1a50397c22bea
abed-naanaa <abednaanaa12@gmail.com>
2025-04-06 13:47:06 +0000

    1

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Defines a class Square with size validation."""
 
+
 class Square:
     """Represents a square with a validated size.
 
-    The size is validated to be an integer and greater than or equal to 0.
+    The size is validated to be an integer andequal to 0.
     """
 
     def __init__(self, size=0):
-        """Initializes the square with a given size and validates it.
+        """Initializes the square with a give.
 
         Args:
             size (int): The size of the square, default is 0.
-        
+
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
