42467c928bfad3ee2ff8a98a2cb21daf15c1dedc
abed-naanaa <abednaanaa12@gmail.com>
2025-04-06 13:44:18 +0000

    f

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Defines an empty class Square."""
 
+
 class Square:
     """Represents an empty square class.
 
-    This class does not have any attributes.
-    placeholder for future sq.
+    This class does not have any attributes or methods.
+    It serves as a placeholder for.
     """
     pass
