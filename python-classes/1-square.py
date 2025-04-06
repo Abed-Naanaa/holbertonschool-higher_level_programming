a96565dfcb175a3a6b7c5191e6a397a84b663f24
abed-naanaa <abednaanaa12@gmail.com>
2025-04-06 06:57:05 +0000

    0-6

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance with a given size."""
+        self.__size = size
