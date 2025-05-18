168f7ee4faba81167bd20caf630e5467a59e7598
abed-naanaa <abednaanaa12@gmail.com>
2025-05-18 09:55:15 +0000

    10-16

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
+-- list the number of records grouped by score, ordered by number of records descending
+
 SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
+-- listing all records with a name from second_table in descending order of score
+
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name != ''
